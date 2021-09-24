d2a736ed31ecc79f117ac800676579811e859012
nikhilnitrr <nikhilchandrakar3@gmail.com>
2021-09-24 17:15:51 +0530

    fix: final changes

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 var btn=document.querySelector('#btn-primary');
 var input=document.querySelector('#input-txt');
 var output=document.querySelector('#output');
+var url='https://api.funtranslations.com/translate/valyrian.json';
+
+
+function generateUrl(text)
+{
+    return url+'?text='+text;
+}
 
 function myScript()
 {
-    output.innerText=input.value;
+    fetch(generateUrl(input.value))
+    .then(response=>response.json())
+    .then(json=>output.innerText=json.contents.translated)
+    .catch(e=>console.log(e))
 }
 
 btn.addEventListener('click',myScript);
